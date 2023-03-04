571063f305047a5c4960065a1b1fcd2f4ba87290
vlnaresh28 <vlnareshreddy@gmail.com>
2023-03-04 05:52:52 +0000

    roboinfra

--- ec2/main.tf
+++ ec2/main.tf
@@ -26,7 +26,7 @@ resource "null_resource" "provisioner" {
     }
 
     inline = [
-      "ansible-pull -i localhost, -U https://github.com/raghulearndevopseasy/roboshop-ansible roboshop.yml -e role_name=${var.component}"
+      "ansible-pull -i localhost, -U https://https://github.com/vlnaresh28/roboshop-ansible roboshop.yml -e role_name=${var.component}"
     ]
 
   }
