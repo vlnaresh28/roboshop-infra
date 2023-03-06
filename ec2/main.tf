f5d003f52040d4f155f26a096ec23dd1be75f963
vlnaresh28 <vlnareshreddy@gmail.com>
2023-03-06 14:52:24 +0000

    g

--- ec2/main.tf
+++ ec2/main.tf
@@ -1,3 +1,17 @@
+data "aws_caller_identity" "current" {}
+
+data "aws_ami" "ami" {
+  most_recent = true
+  name_regex = "devops-practice-with-ansible"
+  owners = [data.aws_caller_identity.current.account_id]
+}
+
+
+
+
+
+
+
 resource "aws_instance" "ec2" {
   ami                    = data.aws_ami.ami.image_id
   instance_type          = var.instance_type
@@ -57,57 +71,9 @@ resource "aws_route53_record" "record" {
   records = [aws_instance.ec2.private_ip]
 }
 
-resource "aws_iam_policy" "ssm-policy" {
-  name        = "${var.env}-${var.component}-ssm"
-  path        = "/"
-  description = "${var.env}-${var.component}-ssm"
+variable "component" {}
+variable "instance_type" {}
+variable  "env" {
 
-  policy = jsonencode({
-    "Version" : "2012-10-17",
-    "Statement" : [
-      {
-        "Sid" : "VisualEditor0",
-        "Effect" : "Allow",
-        "Action" : [
-          "ssm:GetParameterHistory",
-          "ssm:GetParametersByPath",
-          "ssm:GetParameters",
-          "ssm:GetParameter"
-        ],
-        "Resource" : "arn:aws:ssm:us-east-1:739561048503:parameter/${var.env}.${var.component}*"
-      },
-      {
-        "Sid" : "VisualEditor1",
-        "Effect" : "Allow",
-        "Action" : "ssm:DescribeParameters",
-        "Resource" : "*"
-      }
-    ]
-  })
+defualt = "dev"
 }
-
-resource "aws_iam_role" "role" {
-  name = "${var.env}-${var.component}-role"
-
-  assume_role_policy = jsonencode({
-    "Version" : "2012-10-17",
-    "Statement" : [
-      {
-        "Effect" : "Allow",
-        "Principal" : {
-          "Service" : "ec2.amazonaws.com"
-        },
-        "Action" : "sts:AssumeRole"
-      }
-    ]
-  })
-}
-
-resource "aws_iam_instance_profile" "profile" {
-  name = "${var.env}-${var.component}-role"
-  role = aws_iam_role.role.name
-}
-
-resource "aws_iam_role_policy_attachment" "policy-attach" {
-  role       = aws_iam_role.role.name
-  policy_arn = aws_iam_policy.ssm-policy.arn
