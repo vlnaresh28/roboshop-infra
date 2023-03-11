cd3c1015bf554dd78381df162fe6b5fb05eff565
vlnaresh28 <vlnareshreddy@gmail.com>
2023-03-11 15:16:17 +0000

    vx

---
+++ env-prod/state.tfvars
@@ -0,0 +1,3 @@
+bucket = "terraform-b71"
+key    = "prod/infra/terraform.tfstate"
+region = "us-east-1"
--- main.tf
+++ main.tf
@@ -3,5 +3,6 @@ module "ec2" {
   source        = "./ec2"
   component     = each.value["name"]
   instance_type = each.value["type"]
-
+  env           = var.env
+  monitor       = try(each.value["monitor"], false)
 }
