68696bf1b82dec55e0646bfb834098bf41639e3f
vlnaresh28 <vlnareshreddy@gmail.com>
2023-03-19 18:48:10 +0000

    v

--- main.tf
+++ main.tf
@@ -1,12 +1,8 @@
 module "vpc" {
-  source              = "git::https://github.com/raghudevopsb71/tf-module-vpc.git"
-  env                 = var.env
-  tags                = var.tags
-  default_route_table = var.default_route_table
-  default_vpc_id      = var.default_vpc_id
-
-  for_each        = var.vpc
-  vpc_cidr        = each.value["vpc_cidr"]
-  public_subnets  = each.value["public_subnets"]
-  private_subnets = each.value["private_subnets"]
+  source = "git::https://github.com/vlnaresh28/tf-module-vpc.git"
+  env = var.env
+  
+  for_each = var.vpc
+  vpc_cidr = each.value["vpc_cidr"]
+  
 }
--- vars.tf
+++ vars.tf
@@ -1,5 +1,3 @@
 variable "env" {}
-variable "tags" {}
-variable "default_vpc_id" {}
-variable "default_route_table" {}
+
 variable "vpc" {}
