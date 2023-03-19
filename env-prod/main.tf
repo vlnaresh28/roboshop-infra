68014e34fb6560e816e027f38719e0c16bec2386
vlnaresh28 <vlnareshreddy@gmail.com>
2023-03-19 13:49:21 +0000

    j

--- env-prod/main.tf
+++
@@ -1,7 +0,0 @@
-module "ec2" {
-    for_each     = var.instances
-    source       = "git::https://github.com/vlnaresh28/tf-module-vpc.git"
-    cidr         = each.value ["cidr"]
-    env          = var.vpc
- 
-}
